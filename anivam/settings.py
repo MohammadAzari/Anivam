9d2c003469cd892d23bead8ffc7ff7a195bb473e
MohammadAzari <mazard@protonmail.com>
2020-06-07 23:20:25 +0430

    change INSTALLED_APP in settings.py

--- anivam/settings.py
+++ anivam/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'anivamSite.apps.AnivamsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
