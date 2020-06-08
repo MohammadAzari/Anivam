22915aa3ed9848499104702e532f49eeda5871bb
MohammadAzari <mazard@protonmail.com>
2020-06-08 11:37:10 +0430

    change main urls (include app url and ...)

--- anivam/urls.py
+++ anivam/urls.py
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from anivamSite import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^anivam/',include('anivamSite.urls')),
+]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns += [
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/profile/', views.Profile, name='profile'),
 ]
