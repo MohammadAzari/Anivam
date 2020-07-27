8f9c302d328e1d711d3d9ac0f87bf1a55a31b718
ahamiri1377 <ah.amiri1377@gmail.com>
2020-07-27 12:20:29 +0430

    chat room bug fixed!

--- anivamSite/views.py
+++ anivamSite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Person
 from django.contrib.auth.models import User
@@ -21,36 +21,32 @@ def profile(request, username):
     else:
         its_me = True
 
-    u = User.objects.get(username=username)
-    person = Person.objects.get(user=u)
-
-    p = Person.objects.get(user=request.user)
-
-    if person in p.followers.all():
-        is_follow = True
+    user_check = User.objects.filter(username=username)
+    if len(user_check) == 0:
+        return HttpResponse("User not found!")
     else:
-        is_follow = False
-    
-    if request.method == 'POST':
-        key = request.POST.get('key')
-        if key == "want_unfollow":
-            p.followers.remove(person)
-            person.followings.remove(p)
-        elif key == "want_follow":
-            p.followers.add(person)
-            person.followings.add(p)
-    # following = None
-    # followers = None
-    # credit = None
-    # # if request.user.is_authenticated:
-    #     followers_numb = len(request.user.followers.all())
-    #     followings_numb = len(request.user.followings.all())
-    #     credit = request.user.credit
-    # {'following':followers_numb, 'followings':followings_numb, 'credit':credit}
-    content = {
-        'username':username,
-        'its_me':its_me,
-        "person":person,
-        'is_follow':is_follow,
-    }
-    return render(request, 'anivamSite/profile.html', content)
+        u = user_check[0]
+        person = Person.objects.get(user=u)
+
+        p = Person.objects.get(user=request.user)
+
+        if person in p.followers.all():
+            is_follow = True
+        else:
+            is_follow = False
+        
+        if request.method == 'POST':
+            key = request.POST.get('key')
+            if key == "want_unfollow":
+                p.followers.remove(person)
+                person.followings.remove(p)
+            elif key == "want_follow":
+                p.followers.add(person)
+                person.followings.add(p)
+        content = {
+            'username':username,
+            'its_me':its_me,
+            "person":person,
+            'is_follow':is_follow,
+        }
+        return render(request, 'anivamSite/profile.html', content)
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.utils.safestring import mark_safe
+from .models import Messages
 
 import json
 
@@ -8,8 +9,16 @@ def basic_chat(request):
     return render(request, 'chat/basic_chat.html', {})
 
 @login_required 
-def room(request, room_name):   
+def room(request, room_name):  
+    allm = Messages.objects.all()
+    all_rooms = []
+    for r in allm:
+        all_rooms.append(r.room)
+    if not room_name in all_rooms:
+        all_rooms.append(room_name)
+
     return render(request, 'chat/room.html', {
         'room_name': mark_safe(json.dumps(room_name)),
-        'username': mark_safe(json.dumps(request.user.username))
+        'username': mark_safe(json.dumps(request.user.username)),
+        'all_rooms':all_rooms,
     })
